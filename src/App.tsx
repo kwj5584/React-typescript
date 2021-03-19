8cc271900b4b67bb76ec6d5743889a1719d655e1
kwj5584 <kvvj5584@gmail.com>
2021-03-19 13:55:11 +0900

    react-typescript component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+
 import './App.css';
 
-function App() {
+export interface AppProps {
+  name : string
+}
+interface AppState {
+  age:number
+}
+
+class App extends React.Component< AppProps, AppState> {
+  // public state:{age:number}={
+  //   age : 30
+  // } state 방법 1 : state선언만 하기
+  constructor(props:AppProps){
+    super(props);
+    this.state={
+      age:30
+    }
+    setInterval(()=>{
+      this.setState({
+        age:this.state.age +1
+      })
+    },2000);
+  } // state 방법2 : constructor 사용하고 props바인딩
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.props.name}, {this.state.age}
+      <StatelessComponent name="Anna">나는 자식이다.</StatelessComponent>
     </div>
+  );
+}
+}
+
+//stateless component (functinal component)
+const StatelessComponent:React.SFC<AppProps> = (props) =>{
+  return(
+    <h2>{props.name} {props.children}</h2>
   );
 }
 
